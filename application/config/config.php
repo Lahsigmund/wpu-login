f0590a25bbe8248eb50b35de977db6848d08878f
SIGMUND <121880744+Lahsigmund@users.noreply.github.com>
2024-07-18 21:48:38 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://lahsigmund.github.io/wpu-login/';
+$config['base_url'] = 'https://lahsigmund.github.io/wpu-login/index.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://lahsigmund.github.io/wpu-login/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'https://lahsigmund.github.io/wpu-login/index.php';
 
 /*
 |--------------------------------------------------------------------------
